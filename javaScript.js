2f867c3dba9c19fcf3b54989d3379c5d9541f48d
jean kroth <152173490+krothjean@users.noreply.github.com>
2024-04-09 11:48:23 -0600

    alterações dia 9/04 (terça-feira)

---
+++ javaScript.js
@@ -0,0 +1,7 @@
+var inputElement = document.querySelector("input")
+var listElement = document.querySelector("ul")
+var itemElement = listElement.querySelectorAll("li")
+
+inputElement.addEventListener("input", (e) => {
+    // Aqui irá executar em toda alteração no input
+  })
