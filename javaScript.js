059e79e42eb866ca88cece912491617753bd743d
jean kroth <152173490+krothjean@users.noreply.github.com>
2024-04-11 08:47:45 -0600

    atualização 11/04 (quinta-feira)

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,30 @@
-var inputElement = document.querySelector("input")
-var listElement = document.querySelector("ul")
-var itemElement = listElement.querySelectorAll("li")
+//let inputElement = document.querySelector("input")
+//let listElement = document.querySelector("ul")
+//let itemElement = listElement.querySelectorAll("li")
 
-inputElement.addEventListener("input", (e) => {
+//inputElement.addEventListener("input", (e) => {
     // Aqui irá executar em toda alteração no input
-  })
+  //})
+
+  //inputElement.addEventListener("input", (e) => {
+    //itemElement.forEach((li) => {
+      // Função executada para cada item da lista
+    //})
+  //})
+
+  //let inputed = e.target.value.toLowerCase()
+  //inputElement.addEventListener("input", (e) => {
+    //let inputed = e.target.value.toLowerCase()
+    //itemElement.forEach((li) => {
+      //let text = li.textContent.toLowerCase()
+      //if(text.includes(inputed)){
+        //li.style.display = "block"
+      //}else{
+        //li.style.display = "none"
+      //}
+    //})
+  //})
+
+  function Adicionar(){
+    
+  }
