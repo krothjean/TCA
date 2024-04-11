7927d8797a70d1bb4035239f87539aa8154d7435
jean kroth <152173490+krothjean@users.noreply.github.com>
2024-04-11 11:51:19 -0600

    atualização 11/04 quinta_feira

--- javaScript.js
+++ javaScript.js
@@ -1,30 +1,41 @@
-//let inputElement = document.querySelector("input")
-//let listElement = document.querySelector("ul")
+
+//let inputElement = document.querySelector(".procura_padaria")//input
+//let listElement = document.querySelector(".produos+padaria")//ul)
 //let itemElement = listElement.querySelectorAll("li")
 
 //inputElement.addEventListener("input", (e) => {
-    // Aqui irá executar em toda alteração no input
-  //})
-
-  //inputElement.addEventListener("input", (e) => {
-    //itemElement.forEach((li) => {
-      // Função executada para cada item da lista
-    //})
-  //})
-
   //let inputed = e.target.value.toLowerCase()
-  //inputElement.addEventListener("input", (e) => {
-    //let inputed = e.target.value.toLowerCase()
-    //itemElement.forEach((li) => {
-      //let text = li.textContent.toLowerCase()
-      //if(text.includes(inputed)){
-        //li.style.display = "block"
-      //}else{
-        //li.style.display = "none"
-      //}
-    //})
+  //itemElement.forEach((li) => {
+    //let text = li.textContent.toLowerCase()
+    //if(text.includes(inputed)){
+      //li.style.display = "block"
+    //}else{
+      //li.style.display = "none"
+    //}
   //})
+//})
 
-  function Adicionar(){
+  //function Adicionar(){
     
-  }
+  //}
+  //function CarrinhoCompras(){
+    
+  //}
+  const barraDePesquisa = document.querySelector("#pesquisa")//<input/>
+const lista = document.querySelector("#lista");//<ul></ul>
+const itens = document.querySelectorAll("#lista__item");//<li><li/>
+
+barraDePesquisa.addEventListener("input", ()=>{
+    let valorDigitado = barraDePesquisa.value.toLowerCase();
+    itens.forEach((item)=>{
+         if(item.textContent.toLowerCase().includes(valorDigitado)){
+            item.style.display = "block"
+         } else {
+             item.style.display = "none"
+        }
+
+         if(valorDigitado == ""){
+            item.style.display = "block"
+         }
+    })
+})
