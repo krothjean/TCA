d5199f5350508f291dc07c5d34448a4b6b040655
souzzaleo <leosantos.souzza@gmail.com>
2024-04-16 18:04:42 -0300

    atualizaçao

--- javaScript.js
+++ javaScript.js
@@ -2,6 +2,23 @@ function Comprar(){
  var quantidade = "-"
  var quantidade = "+"
 }
-function Adicionar_ao_Carrinho{
+function Adicionar_ao_Carrinho(){
+
+}
+function pesquisar(){
+
+}
+function Search(){
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase()
+    let x = document.getElementsByClassName('alimentos')
+    
+    for(i = 0; i < x.length; i++){
+        if(!x[i].innerHTML.toLowerCase().includes(input)){
+            x[i].style.display  = "none"
+        }else{
+            x[i].style.display = "list-item"
+        }
+    }
 
 }
